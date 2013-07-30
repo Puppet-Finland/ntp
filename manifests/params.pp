3cd87f0e3691173d43dabf3c1c801a022d178cb7
Samuli Seppänen <samuli@openvpn.net>
2013-07-30 11:14:51 +0300

    Fixed faulty service command in params.pp
    RedHat family of operating systems was using a faulty service command which
    prevented monit from working properly. Fixed this issue and cleaned up params.pp
    in general.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,16 +5,27 @@
 #
 class ntp::params {
 
-    $service_name = $::osfamily ? {
-        'RedHat' => 'ntpd',
-        'Debian' => 'ntp',
-        default  => 'ntp',
+    case $::osfamily {
+        'RedHat': {
+            $service_name = 'ntpd'
+            $service_command = "/sbin/service $service_name"  
+        }
+        'Debian': {
+            $service_name = 'ntp'
+            $service_command = "/usr/sbin/service $service_name"
+        }
+        'Ubuntu': {
+            $service_name = 'ntp'
+            $service_command = "/usr/sbin/service $service_name"
+        }
+        default: {
+            $service_name = 'ntp'
+            $service_command = "/usr/sbin/service $service_name"
+        }
     }
 
-    $service_command = $::osfamily ? {
-        default => "/usr/sbin/service $service_name",
-    }
-
+    # This can be used to work around startup scripts that don't have a proper
+    # "status" target.
     $service_hasstatus = $::lsbdistcodename ? {
         default => 'true',
     }
